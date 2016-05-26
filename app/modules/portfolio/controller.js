549ed291a7dee7ab8b42b69fd3e4c45f74731ec1
Nancy Majors <nmajors@gmail.com>
2016-05-26 15:53:28 -0500

    updated image path for published site

--- app/modules/portfolio/controller.js
+++ app/modules/portfolio/controller.js
@@ -3,28 +3,28 @@ class PortfolioController {
     this.projects=[
       {
         name: "Weather Watcher",
-        image: "../assets/images/weather-watcher-full.png",
+        image: "./assets/images/weather-watcher-full.png",
         description: "My final project",
         site: "https://nmajors.github.io/final-project/",
         repository: "https://github.com/nmajors/final-project.git"
       },
       {
         name: "Powell Peralta Skateboards",
-        image: "../assets/images/powell-peralta-full.png",
+        image: "./assets/images/powell-peralta-full.png",
         description:  "A fully responsive layout assignment",
         site: "http://nmajors.github.io/08-powell-peralta-master/",
         repository: "https://github.com/nmajors/08-powell-peralta-master.git"
       },
       {
         name: "To Do List",
-        image: "../assets/images/to-do-list.jpg",
+        image: "./assets/images/to-do-list.jpg",
         description: "A simple to do list app",
         site: "http://nmajors.github.io/28-firebase/dist/",
         repository: "https://github.com/nmajors/28-firebase.git"
       },
       {
         name: "Image Gallery",
-        image: "../assets/images/image-gallery.jpg",
+        image: "./assets/images/image-gallery.jpg",
         description: "A gallery app using services and Github authentication",
         site: "http://nmajors.github.io/29-auth-services/dist/",
         repository: "https://github.com/nmajors/29-auth-services.git"
