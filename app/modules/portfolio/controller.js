4ef0cd5804a7b773cc5b9f765ba16cbfbcce4598
Nancy Majors <nmajors@gmail.com>
2016-05-27 00:44:20 -0500

    animation on portfolio section

--- app/modules/portfolio/controller.js
+++ app/modules/portfolio/controller.js
@@ -1,36 +1,51 @@
 class PortfolioController {
   constructor(){
-    this.projects=[
+    this.project1 =
       {
         name: "Weather Watcher",
         image: "./assets/images/weather-watcher-full.png",
         description: "My final project",
         site: "https://nmajors.github.io/final-project/",
         repository: "https://github.com/nmajors/final-project.git"
-      },
+      };
+
+    this.project2 =
       {
         name: "Powell Peralta Skateboards",
         image: "./assets/images/powell-peralta-full.png",
         description:  "A fully responsive layout assignment",
         site: "http://nmajors.github.io/08-powell-peralta-master/",
         repository: "https://github.com/nmajors/08-powell-peralta-master.git"
-      },
+      };
+
+    this.project3 =
       {
         name: "Image Gallery",
         image: "./assets/images/image-gallery.jpg",
         description: "A gallery app using services and Github authentication",
         site: "http://nmajors.github.io/29-auth-services/dist/",
         repository: "https://github.com/nmajors/29-auth-services.git"
-      },
+      };
+
+    this.project4 =
       {
         name: "To Do List",
         image: "./assets/images/to-do-list.jpg",
         description: "A simple to do list app",
         site: "http://nmajors.github.io/28-firebase/dist/",
         repository: "https://github.com/nmajors/28-firebase.git"
-      }
+      };
 
-    ];
+    this.project5 =
+      {
+        name: "Weather API",
+        image: "./assets/images/weather-api.jpg",
+        description: "An app created almost entirely with vanilla JavaScript to display the current weahter and seven day forecast for the user's current location",
+        site: "https://nmajors.github.io/18-weather-api/dist/",
+        repository: "https://github.com/nmajors/18-weather-api.git"
+      };
+
+
 
   }
 }
