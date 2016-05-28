dc729d5a8b8aac133529dfeb2bdea7b989bca69c
Nancy Majors <nmajors@gmail.com>
2016-05-28 13:27:05 -0500

    angular-inview

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import uiRouter from 'angular-ui-router';
 
 import firebase from 'firebase';
 import angularFire from 'angularfire';
+import angularInview from 'angular-inview';
 
 
 
@@ -11,6 +12,7 @@ import portfolio from './modules/portfolio';
 let App = angular.module('app', [
   'ui.router',
   'firebase',
+  'angular-inview',
 
 
   'nms.portfolio'
--- app/modules/portfolio/controller.js
+++ app/modules/portfolio/controller.js
@@ -1,9 +1,25 @@
 class PortfolioController {
   constructor(){
-    
+
+
+    this.descriptions = [ "User adds the addresses of their friends and family across the country. The app stores the user's list of locations and when the user logs on, the current weather is displayed for each saved location, along with the user's current position. Animations call attention to severe weather situations.",
+    "Demonstrates the ability to create a responsive page layout from scratch using Sass variables and nesting, effectively using node-sass and @media queries.",
+    "Moving business logic and data calls into a service, using simple authorization through github so each user has their own data. Using services to organize code.",
+    "Using Firebase to act as a simple back end for a todo list app, saves todos, can mark todos as complete, can view all complete or all incomplete, and can delete completed todos.",
+    "Using GET to receive data from the OpenWeatherMap API, hiding the API key through a proxy, effectively using Promise and fetch to get weather data for current position and creating an the app almost entirely in vanilla Javascript. Geolocation for current position."
+      ]
+
+    this.bouncing = false;
+
     }
+
+
+  isInView(inview){
+    this.bouncing = true;
+    console.log(this.bouncing);
+  }
+
+
 }
-
-
 
 export default PortfolioController;
